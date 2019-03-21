ece57e57342da6bd369ffd4ff79b30765f18462f
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-21 17:31:23 +0100

    Server setup

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const server = require('./api/server.js');
 const PORT = process.env.PORT || 5000;
+
+console.log(process.env.MY_LITTLE_SECRET);
 
 server.listen(PORT, () => {
   console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
